8ec1a1487aaddc2d375e5ee73e51a2d0d768fcdc
Sarp Bozkurt <sarpbozk@gmail.com>
2024-06-05 20:53:42 +0300

    Refactored code with modular components and price calculation logic for beije ped section

--- BeijeTechTest/App.js
+++ BeijeTechTest/App.js
@@ -1,49 +1,55 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  ScrollView,
-  SafeAreaView,
-} from "react-native";
-import Slider from "@react-native-community/slider";
+import React, { useState, useMemo } from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 import Header from "./components/Header";
 import Summary from "./components/Summary";
 import Tab from "./components/Tab";
 import SliderComponent from "./components/SliderComponent";
-import usePadState from "./hooks/usePadState";
 
 const App = () => {
-  const {
-    standardPad,
-    setStandardPad,
-    superPad,
-    setSuperPad,
-    superPlusPad,
-    setSuperPlusPad,
-  } = usePadState(50, 20, 50);
+  const [quantities, setQuantities] = useState({
+    "Standart Ped": 0,
+    "Süper Ped": 0,
+    "Süper+ Ped": 0,
+  });
+
+  const priceMap = {
+    "Standart Ped": [0, 60.84, 119.85, 189.41, 239.7, 299.76, 358.82],
+    "Süper Ped": [0, 71.4, 140.01, 208.42, 279.82, 348.43, 416.84],
+    "Süper+ Ped": [0, 80.03, 158.21, 235.74, 315.77, 393.95, 471.48],
+  };
+
+  const handleValueChange = (label, quantity) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [label]: quantity,
+    }));
+  };
+
+  const totalPrice = useMemo(() => {
+    return (
+      priceMap["Standart Ped"][quantities["Standart Ped"] / 10] +
+      priceMap["Süper Ped"][quantities["Süper Ped"] / 10] +
+      priceMap["Süper+ Ped"][quantities["Süper+ Ped"] / 10]
+    );
+  }, [quantities]);
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView style={styles.container}>
         <Header />
-        <Summary />
+        <Summary quantities={quantities} totalPrice={totalPrice} />
         <Tab />
         <SliderComponent
           label="Standart Ped"
-          value={standardPad}
-          onValueChange={setStandardPad}
+          onValueChange={(value) => handleValueChange("Standart Ped", value)}
         />
         <SliderComponent
           label="Süper Ped"
-          value={superPad}
-          onValueChange={setSuperPad}
+          onValueChange={(value) => handleValueChange("Süper Ped", value)}
         />
         <SliderComponent
           label="Süper+ Ped"
-          value={superPlusPad}
-          onValueChange={setSuperPlusPad}
+          onValueChange={(value) => handleValueChange("Süper+ Ped", value)}
         />
       </ScrollView>
     </SafeAreaView>
--- BeijeTechTest/components/SliderComponent.js
+++ BeijeTechTest/components/SliderComponent.js
@@ -1,25 +1,38 @@
-// components/SliderComponent.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Slider from "@react-native-community/slider";
 
-const SliderComponent = ({ label, value, onValueChange }) => (
-  <View style={styles.sliderContainer}>
-    <Text style={styles.sliderLabel}>{label}</Text>
-    <Slider
-      style={styles.slider}
-      minimumValue={0}
-      maximumValue={60}
-      step={1}
-      value={value}
-      minimumTrackTintColor="#000000"
-      maximumTrackTintColor="#d3d3d3"
-      thumbTintColor="#000000"
-      onValueChange={onValueChange}
-    />
-    <Text style={styles.sliderValue}>{value}</Text>
-  </View>
-);
+const priceMap = {
+  "Standart Ped": [0, 60.84, 119.85, 189.41, 239.7, 299.76, 358.82],
+  "Süper Ped": [0, 71.4, 140.01, 208.42, 279.82, 348.43, 416.84],
+  "Süper+ Ped": [0, 80.03, 158.21, 235.74, 315.77, 393.95, 471.48],
+};
+
+const SliderComponent = ({ label, onValueChange }) => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    onValueChange(value, priceMap[label][value / 10]);
+  }, [value]);
+
+  return (
+    <View style={styles.sliderContainer}>
+      <Text style={styles.sliderLabel}>{label}</Text>
+      <Slider
+        style={styles.slider}
+        minimumValue={0}
+        maximumValue={60}
+        step={10}
+        value={value}
+        minimumTrackTintColor="#000000"
+        maximumTrackTintColor="#d3d3d3"
+        thumbTintColor="#000000"
+        onValueChange={(val) => setValue(val)}
+      />
+      <Text style={styles.sliderValue}>{value}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   sliderContainer: {
